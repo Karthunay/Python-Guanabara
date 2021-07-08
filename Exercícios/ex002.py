edaa7e9b1d7d4a9eea2b4a26029608750719dce2
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-07 21:03:20 -0300

    Exercícios

--- Exercícios/ex002.py
+++ Exercícios/ex002.py
@@ -1,3 +1,5 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
 nome = input('Qual o seu nome?')
-print('Olá,', nome,'. Seja bem vinda ao melhor ano da sua vida!')
-print(f'Olá {nome}. Seja bem vinda ao melhor ano da sua vida!')
+print('Olá,', nome,'! Seja bem vinda ao melhor ano da sua vida!')
+print(f'Olá, {nome}! Seja bem vinda ao melhor ano da sua vida!')
+print('Olá, {}! Seja bem vinda ao melhor ano da sua vida!'.format(nome))
