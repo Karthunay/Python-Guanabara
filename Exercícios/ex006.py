6dd5a60d1bfff166b61b6c908051fef4db5311cb
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-07 21:59:45 -0300

    Exercícios

--- Exercícios/ex006.py
+++ Exercícios/ex006.py
@@ -1,4 +1,6 @@
-import math
+#Crie um algorítmo que leia um número e mostre o seu dobro, triplo e raíz quadrada
+
+from math import sqrt
 
 n1 = int(input('Digite um número:'))
-print(f'O dobro de {n1} é:{n1*2};\nO triplo é {n1*3};\nE a raiz quadrada é {math.sqrt(n1):.0f} ou {n1**(1/2)}')
+print(f'O dobro de é {n1*2};\nO triplo é {n1*3};\nE a raiz quadrada é {sqrt(n1):.0f} ou {n1**(1/2):.0f}')
