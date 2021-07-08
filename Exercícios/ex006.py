88cd0b11334ea6151d670330a58b3bc6f8694e0f
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-07 22:02:05 -0300

    Exercícios

--- Exercícios/ex006.py
+++ Exercícios/ex006.py
@@ -3,4 +3,4 @@
 from math import sqrt
 
 n1 = int(input('Digite um número:'))
-print(f'O dobro de é {n1*2};\nO triplo é {n1*3};\nE a raiz quadrada é {sqrt(n1):.0f} ou {n1**(1/2):.0f}')
+print(f'O dobro é {n1*2};\nO triplo é {n1*3};\nE a raiz quadrada é {sqrt(n1):.0f} ou {n1**(1/2):.0f}')
