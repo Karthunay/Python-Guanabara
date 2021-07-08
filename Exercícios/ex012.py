afbdd823253b0bdcbc6503a77e6955685066283c
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-08 12:57:55 -0300

    Exercícios

--- Exercícios/ex012.py
+++ Exercícios/ex012.py
@@ -1,7 +1,9 @@
-preço = float(input('Digite o preço do produto:R$'))
-desconto = float(input('Digite o percentual de desconto:'))
-print(f'O produto sairá por R${preço-(preço*desconto/100):.2f}')
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+preco = float(input('Digite o preço do produto: R$'))
+desconto = float(input('Digite o percentual de desconto: '))
+print(f'O produto sairá por R${preco-(preco*desconto/100):.2f}')
 #or
-print(f'O produto com desconto sairá por R${preço*0.95}')
+print(f'O produto com desconto sairá por R${preco*0.95}')
 #or
-print(f'O produto sairá por R${preço-preço*0.05:.2f}')
+print(f'O produto sairá por R${preco-preco*0.05:.2f}')
