b6e9277b52ff097098768d56f39367b7f853c496
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-08 13:33:46 -0300

    Exercícios

--- Exercícios/ex015.py
+++ Exercícios/ex015.py
@@ -1,7 +1,9 @@
+#Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60.00 por dia e R$0.15 por Km rodado.
+
 kmCarro = float(input('Digite os KM percorrido pelo carro alugado: '))
 diasCarro = int(input('Digite quantos dias o carro foi alugado: '))
-diasCarro = diasCarro*60.00
-kmCarro = kmCarro*0.15
-print(f'O custo final de locação foi de R${diasCarro+kmCarro:.2f}')
-
-
+dias = diasCarro*60.00
+km = kmCarro*0.15
+print(f'O custo final de locação foi de R${dias+km:.2f}')
+#or
+print(f'Serão {diasCarro} dias totais de locação a R${diasCarro*60:.2f}, com o total de {kmCarro}km percorridos a R${kmCarro*0.15:.2f}. O custo total será de R${kmCarro*0.15+diasCarro*60:.2f} ')
