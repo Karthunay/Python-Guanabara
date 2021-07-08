2073d8ff2bd743df27d76187784044cb4ab8db8c
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-07 21:33:52 -0300

    Exercícios

--- Exercícios/ex004.py
+++ Exercícios/ex004.py
@@ -1,3 +1,5 @@
+#Faça um programa que leia algo pelo teclado e mostre o seu tipo primitivo e todas as informações possíveis sobre ele.
+
 k = input('Digite algo:')
 print(type(k))
 print(f'Tem espaço? {k.isspace()}')
@@ -7,7 +9,7 @@ print(f'É alfabético? {k.isalpha()}')
 print(f'Está minúsculo? {k.islower()}')
 print(f'Está maiúsculo? {k.isupper()}')
 print(k.isascii())
-print(f'É um dgito? {k.isdigit()}')
+print(f'É um digito? {k.isdigit()}')
 print(f'É um código identificador? {k.isidentifier()}')
 print(f'É imprimivel {k.isprintable()}')
 print(f'Está captalizada/Maiuscula e minuscula? {k.istitle()}')
