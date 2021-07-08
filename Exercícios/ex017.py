a7b2a7eac1a66a4db81b42d5b2b2a9b3b270c72e
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-08 13:57:13 -0300

    Exercícios

--- Exercícios/ex017.py
+++ Exercícios/ex017.py
@@ -1,5 +1,12 @@
-import math
-catOposto = int(input('Digite o comprimento do cateto oposto: '))
-catAdjacente = int(input('Digite o comprimento do cateto adjacente: '))
-hipotenusa = math.sqrt(math.pow(catOposto, 2) + math.pow(catAdjacente, 2))
-print(f'A hipotenusa é {hipotenusa}')
+#Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo. Calcule e mostre o comprimento da hipotenusa.
+
+from math import pow, sqrt, hypot
+catOposto = float(input('Digite o comprimento do cateto oposto: '))
+catAdjacente = float(input('Digite o comprimento do cateto adjacente: '))
+hipotenusa = sqrt(pow(catOposto, 2) + pow(catAdjacente, 2))
+print(f'A hipotenusa é {hipotenusa:.2f}')
+#or
+hipotenusa = hypot (catOposto, catAdjacente)
+print(f'A hipotenusa é {hipotenusa:.2f}')
+#or
+print(f'A hipotenusa é {hypot(catOposto, catAdjacente):.2f}')
