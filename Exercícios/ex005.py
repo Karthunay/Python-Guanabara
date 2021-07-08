1da275051660aac8268058aecf9a835ea506c53b
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-07 21:54:06 -0300

    Exercícios

--- Exercícios/ex005.py
+++ Exercícios/ex005.py
@@ -1,2 +1,5 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor, e seu antecessor.
+
 n1 = int(input('Digite um número:'))
-print(f'O Antecessor de {n1} é {n1-1}, e o sucessor é {n1+1}')
+print(f'O antecessor de {n1} é {n1-1}, e o sucessor é {n1+1}')
+print('O antecessor de {} é {}, e o sucessor é {}'.format(n1, (n1-1), (n1+1)))
