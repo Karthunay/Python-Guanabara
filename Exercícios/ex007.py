251548e840fde79d36d3dbaeea2d75302dbfc9d2
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-07 22:05:23 -0300

    Exercícios

--- Exercícios/ex007.py
+++ Exercícios/ex007.py
@@ -1,3 +1,5 @@
-notap = float(input('Digite a sua nota na matéria de português:'))
-notam = float(input('Digite a sua nota na matéria de matematica:'))
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+notap = float(input('Digite a sua nota da matéria de português: '))
+notam = float(input('Digite a sua nota da matéria de matematica: '))
 print(f'A média entre as suas notas de português e matemática é {(notap+notam)/2:.0f}.')
