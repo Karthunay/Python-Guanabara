7800158fbc4d88c9e7647a23aa9bd6d51bd34e8e
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-08 13:04:10 -0300

    Exercícios

--- Exercícios/ex013.py
+++ Exercícios/ex013.py
@@ -1,7 +1,9 @@
-salario = float(input('Digite o seu salário atual:R$'))
+#Faça um algorítimo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+salario = float(input('Digite o seu salário atual: R$'))
 reajuste = float(input('Digite o percentual de reajuste: '))
-print(f'O salário com o reajuste é R${salario*reajuste/100+salario:.2f}')
+print(f'O salário reajustado é R${salario*reajuste/100+salario:.2f}')
 #or
-print(f'O salário reajustado é R${salario*1.15:.2f}')
+print(f'O salário reajustado em 15% é R${salario*1.15:.2f}')
 #or
 print(f'O salário reajustado em 15% é R${salario*0.15+salario:.2f}')
