83cc26e08468dc3adcb1efc854266e3148f64f39
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-07 22:52:01 -0300

    Exercícios

--- Exercícios/ex010.py
+++ Exercícios/ex010.py
@@ -1,2 +1,4 @@
-dim = float(input('Digite quantos reais vc tem:R$'))
-print(f'Convesão para dólares:${dim/3.27:.2f}')
+#Crie um programa que leia o quanto de dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
+
+dinheiro = float(input('Digite quantos reais vc tem: R$'))
+print(f'Convesão para dólares:${dinheiro/5.23:.2f}')
