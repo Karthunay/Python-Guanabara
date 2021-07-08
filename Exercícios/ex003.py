482ee2b38649b70bff11f7b700958aa6a437a80b
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-07 21:21:55 -0300

    Exercícios

--- Exercícios/ex003.py
+++ Exercícios/ex003.py
@@ -1,4 +1,8 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+
 n1 = int(input('Qual a sua idade?'))
 n2 = int(input('Qual a idade do seu irmão?'))
 s = n1 + n2
-print(f'A soma entre a sua idade {n1} e a idade de seu irmão {n2} é {s}')
+print(f'A soma entre a sua idade {n1} e a idade do seu irmão {n2}, é {s}')
+print(f'A soma entre a sua idade {n1} e a idade do seu irmão {n2}, é {n1+n2}')
+print('A soma entre a sua idade {} e a idade do seu irmão {}, é {}.'.format (n1, n2, (n1+n2)))
