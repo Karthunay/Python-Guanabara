7f5ca6acbd9ced8a05a2b179f0187f242be331f7
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-08 14:13:14 -0300

    Exercícios

--- Exercícios/ex018.py
+++ Exercícios/ex018.py
@@ -1,4 +1,12 @@
-import math
-angulo = int(input('Digite o angulo: '))
-cosseno = math.sin(angulo)
-print(f'O cosseno é {cosseno}')
+#Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+
+from math import sin, cos, tan, radians
+angulo = float(input('Digite o angulo: '))
+seno = sin(radians(angulo))
+cosseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
+print(f'O ângulo {angulo} tem as seguintes informações:\n'
+      f'O seno é {seno:.2g}\n'
+      f'O cosseno é {cosseno:.2f}\n'
+      f'A tangente é {tangente:.2f}')
+#or
