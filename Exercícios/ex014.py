3fa32690097fd89987bda094f6e6b9a6abdd071c
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-08 13:12:33 -0300

    Exercícios

--- Exercícios/ex014.py
+++ Exercícios/ex014.py
@@ -1,2 +1,4 @@
-c = float(input('Digite o grau em C°:'))
-print(f'C° {c} é equivalente a °F {c*1.8+32}')
+#Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
+c = float(input('Digite o grau em °C: '))
+print(f' {c} °C  é equivalente a {c*1.8+32} °F')
