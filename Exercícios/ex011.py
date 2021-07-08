4191c0b21cf77e3c79fda9411db9278b294e1959
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-07 23:02:44 -0300

    Exercícios

--- Exercícios/ex011.py
+++ Exercícios/ex011.py
@@ -1,5 +1,10 @@
-lag = float(input('Digite a largura:'))
-alt = float(input('Digite a altura:'))
-area = lag*alt
-print(f'Área:{area:.2f}\n'
-      f'Quantidade de tinta:{area/2:.2f}litro/mt²')
+#Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área, e a quantidade de tinta necessária para pintá-la, sabendo que cada litro  de tinta pinta uma área de 2mt².
+
+largura = float(input('Digite a largura: '))
+altura = float(input('Digite a altura: '))
+area = largura*altura
+print(f'Área: {area:.2f}\n'
+      f'Quantidade de tinta: {area/2:.0f}lt/mt²')
+#or
+print(f'Área: {largura*altura:.2f}\n'
+      f'Quantidade de tinta: {largura*altura/2:.0f}lt/mt²')
