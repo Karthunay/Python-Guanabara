568239875bf6fc37b8eef917e0188c41fff64a3a
Kellen Karthunay <kkarthunay@gmail.com>
2021-07-22 20:52:04 -0300

    Exercícios

---
+++ Exercícios/ex024.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia o nome de uma cidade e diga se la começa ou não com o nome "SANTO"
+
+cidade = str(input('Em que cidade você nasceu? ')).strip()
+print(cidade[:5].upper() == 'SANTO')
